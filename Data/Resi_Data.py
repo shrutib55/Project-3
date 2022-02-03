61bc64daa9ce54094226e58f5cafb5f839d9cb78
shrutib55 <88411429+shrutib55@users.noreply.github.com>
2022-02-03 16:55:28 -0600

    Update Resi_Data.py

--- Data/Resi_Data.py
+++ Data/Resi_Data.py
@@ -136,7 +136,7 @@ if option == 'Graph':
         list_sale = components.html(html_temp, height = 2000)
     elif graph == 'Percent Change Over Time':
         html_temp = """<div class='tableauPlaceholder' id='viz1643928720286' style='position: relative'><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='PercentChange-MedianPrice&#47;Sheet10' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1643928720286');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-        avg_time = components.html(html_temp, height = 1000)
+        avg_time = components.html(html_temp, height = 400)
 
 if option == 'Forecast':
     st.write(
